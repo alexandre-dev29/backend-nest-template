08a435cd5f71971b5783e349dce00a115c4daaa3
alexandre-dev29 <axel.business29@gmail.com>
2022-06-02 13:02:27 +0200

    Cleaning the project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
-import { Module } from "@nestjs/common";
+import { Module } from '@nestjs/common';
 
 @Module({
-  imports: []
+  imports: [],
 })
-export class AppModule {
-}
+export class AppModule {}
