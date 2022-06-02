537786116d5acbd41d0c8231a9f0cc297198245b
alexandre-dev29 <axel.business29@gmail.com>
2022-06-02 18:30:39 +0200

    adding access and refresh token strategy

---
+++ src/auth/strategies/access_token.strategy.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class AccessTokenStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: 'secretKey',
+    });
+  }
+}
